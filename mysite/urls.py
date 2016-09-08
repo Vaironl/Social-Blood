e5a8dadfeb42eca5cbe348d625fbc6f361e716d4
Vairon M <vairon@live.com>
2016-09-08 07:45:01 -0400

    Setup the post and my admin parts of the djangogirls tutorial

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
  	# url(r'^polls/', include('polls.urls')),
- 	url(r'^socialblood/', include('socialblood.urls')),
+ 	url(r'', include('socialblood.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- socialblood/views.py
+++ socialblood/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Social Blood</h1>")
+	return render(request, 'blog/post_list.html', {})
+#    return HttpResponse("<h1>Social Blood</h1>")
