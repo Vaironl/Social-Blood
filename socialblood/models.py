5fb67684453c5f4776ef0ae3f30f920a3c8f5b9c
vaironl <vairon@live.com>
2016-09-19 11:44:45 -0400

    only one milestone test left to pass

--- socialblood/models.py
+++ socialblood/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -19,3 +20,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class UserProfile(models.Model):
+    #link to user model instance
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.user.username
+
--- socialblood/views.py
+++ socialblood/views.py
@@ -1,6 +1,39 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from .forms import UserForm, UserProfileForm
+
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		profile_form = UserProfileForm(data=request.POST)
+
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+
+			user.set_password(user.set_password)
+			user.save()
+
+			profile = profile_form.save(commit=False)
+			profile.user = user
+
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['pictures']
+
+				profile.save()
+				registered = True
+			else:
+				print user_form.errors, profile_form.errors
+		else:
+			user_form = UserForm()
+			profile_form = UserProfileForm()
+
+	return render(request, './templates/blog/register.html',
+	 {'user_form': user_form,
+	  'profile_form':profile_form,
+	  'registered':registered})
 
 
 # def index(request):
