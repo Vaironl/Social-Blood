15ce2b79ef51b4b8a1558152e219595c29cc8cc1
vaironl <vairon@live.com>
2016-11-11 22:38:14 -0500

    Cleaning

--- socialblood/models.py
+++ socialblood/models.py
@@ -5,22 +5,6 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(
-        default=timezone.now)
-    published_date = models.DateTimeField(
-        blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def __str__(self):
-        return self.title
-
 BLOOD_TYPES = ('A+', 'A+'),\
               ('A-', 'A-'),\
               ('B+', 'B+'),\
