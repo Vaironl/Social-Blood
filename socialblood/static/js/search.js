88f93e8a76a7fb846a28d462c3a56bf993b61e25
vaironl <vairon@live.com>
2016-12-21 21:41:10 -0500

    Application is more or less ready to be demoed. Some minor features needed to be added, such as splitting a large number of blood requests over multiple pages and searching through the requests.

--- socialblood/static/js/search.js
+++ socialblood/static/js/search.js
@@ -3,8 +3,10 @@ function searchFunction() {
     var input, filter, ul, li, a, i;
     input = document.getElementById('searchRequestInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("requestUL");
-    li = ul.getElementsByTagName('li');
+    // ul = document.getElementByClassName("row")[0];
+    location = document.getElementByClassName("row")[0];
+    // location = ul.getElementsByTagName('li');
+    
 
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
